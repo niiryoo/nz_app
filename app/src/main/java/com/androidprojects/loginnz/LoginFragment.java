00e750fc095c95b72de66464cb4a36f7cfc3c584
lhw93 <lhw9391@gmail.com>
2023-01-26 17:22:39 +0900

    new

--- app/src/main/java/com/androidprojects/loginnz/LoginFragment.java
+++ app/src/main/java/com/androidprojects/loginnz/LoginFragment.java
@@ -1,77 +1,56 @@
 package com.androidprojects.loginnz;
 
-import android.content.ContentValues;
-import android.content.Intent;
-import android.os.AsyncTask;
+import android.content.Context;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TabHost;
-import android.widget.TextView;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.NetworkResponse;
-import com.android.volley.Request;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.HttpHeaderParser;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-
-import okhttp3.MediaType;
-import okhttp3.RequestBody;
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.List;
 
 public class LoginFragment extends Fragment {
     public EditText edit_id, edit_password;
-
+    String sSSID;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_login, container, false); // fragment_login 레이아웃들(레이아웃, 위젯등)을 객체화 하여 메모리에 올리는 작업.
         edit_id = (EditText) rootView.findViewById(R.id.emp_id);
         edit_password = (EditText) rootView.findViewById(R.id.emp_password);
 
+        sSSID = getWiFiSSID(getActivity()).toString();
+        String SSID = "\"KelvinWiFi\"";
         Button login = rootView.findViewById(R.id.loginbutton); //버튼 지정
         login.setOnClickListener(new View.OnClickListener() { // 클릭 리스너 설정
             @Override
             public void onClick(View view) { // 클릭시 이벤트 설정
-                loginRequest();
+                if(sSSID.equals(SSID)){
+                    loginRequest();
+                }
+                else{
+                    Toast.makeText(getContext(),sSSID,Toast.LENGTH_LONG).show();
+                }
             }
         });
         return rootView;
     }
+
 
     private void loginRequest() { // 로그인 정보 파싱후 처리
         Response.Listener<String> responseListener = new Response.Listener<String>() {
@@ -95,4 +74,12 @@ public class LoginFragment extends Fragment {
         RequestQueue queue = Volley.newRequestQueue(getContext());
         queue.add(LoginPost);
     }
+
+    public String getWiFiSSID(Context mContext)
+    {
+        WifiManager manager = (WifiManager)mContext.getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInfo = manager.getConnectionInfo();
+        return wifiInfo.getSSID();
+    }
+
 }
